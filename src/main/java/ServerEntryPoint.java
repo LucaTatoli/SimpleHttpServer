622f0be0873bdfd68e9211d9eecad3555d74b618
LucaTatoli <luca.tatoli27@gmail.com>
2022-12-24 13:24:20 +0100

    improved request header end detector and body reader

--- src/main/java/ServerEntryPoint.java
+++ src/main/java/ServerEntryPoint.java
@@ -2,16 +2,14 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
-import java.util.concurrent.TimeUnit;
-
-import static javax.swing.text.html.FormSubmitEvent.MethodType.POST;
 
 public class ServerEntryPoint {
     private ServerSocket serverSocket;
     private boolean running = false;
     private HashMap<String, String> headers;
 
-    public ServerEntryPoint(int port) {
+    public ServerEntryPoint(int port)
+    {
         try
         {
             serverSocket = new ServerSocket(port);
@@ -22,13 +20,16 @@ public class ServerEntryPoint {
         headers = new HashMap<>();
     }
 
-    public void listen() {
+    public void listen()
+    {
         running = true;
         Socket socket;
         String newConnectionString = "==========================================\nNew connection from: %s\n==========================================\n\n\n";
-        String endConnectionString = "==========================================\nEnd connection with: %s\n==========================================\n\n\n";
-        while (running) {
-            try {
+        String endConnectionString = "\n\n==========================================\nEnd connection with: %s\n==========================================\n\n\n";
+        while (running)
+        {
+            try
+            {
                 socket = serverSocket.accept();
                 String addr = socket.getInetAddress().toString();
                 System.out.printf(newConnectionString, addr);
@@ -36,25 +37,32 @@ public class ServerEntryPoint {
                 System.out.printf(endConnectionString, addr);
                 socket.close();
                 headers.clear();
-            } catch (IOException e) {
+            }
+            catch (IOException e)
+            {
                 e.printStackTrace();
             }
         }
     }
 
-    public void requestHandler(Socket connection) throws IOException {
+    public void requestHandler(Socket connection) throws IOException
+    {
         String request = getRequest(connection.getInputStream());
-        String http_method = request.substring(0, request.indexOf("\n")).split(" ")[0];
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
-        if (!ApplicationConstants.HTTP_METHODS.contains(http_method))
-            bw.write(HttpResponse.methodNotSupportedResponse());
-        else
-            bw.write(HttpResponse.okResponse());
-        bw.flush();
-        System.out.println(request);
+        if(request.length() > 0)
+        {
+            String http_method = request.substring(0, request.indexOf("\n")).split(" ")[0];
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
+            if (!ApplicationConstants.HTTP_METHODS.contains(http_method))
+                bw.write(HttpResponse.methodNotSupportedResponse());
+            else
+                bw.write(HttpResponse.okResponse());
+            bw.flush();
+            System.out.println(request);
+        }
     }
 
-    public String getRequest(InputStream stream) throws IOException {
+    public String getRequest(InputStream stream) throws IOException
+    {
         StringBuilder request = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(stream));
         getHeaders(br, request);
@@ -65,48 +73,30 @@ public class ServerEntryPoint {
 
     public void getHeaders(BufferedReader br, StringBuilder request) throws IOException
     {
-        String line;
-        boolean headerEnded = false;
         boolean skipFirst = true;
         String[] split;
-        while (!headerEnded) {
-            if (br.ready())
+        String line = br.readLine();
+        while (line != null && line.length() != 0)
+        {
+            System.out.println(line);
+            if(!skipFirst)
             {
-                line = br.readLine();
-                if (line.length() == 0)
-                {
-                    headerEnded = true;
-                }
-                else if(!skipFirst)
-                {
-                    split = line.split(":");
-                    headers.put(split[0], split[1]);
-                }
-                else skipFirst = false;
-                request.append(line);
-                request.append("\n");
+                split = line.split(":");
+                headers.put(split[0].strip(), split[1].strip());
             }
+            else
+                skipFirst = false;
+            request.append(line);
+            request.append("\n");
+            line = br.readLine();
         }
     }
 
     public void getBody(BufferedReader br, StringBuilder request) throws IOException
     {
-        String line;
-        boolean bodyEnded = false;
-        while (!bodyEnded) {
-            if (br.ready())
-            {
-                line = br.readLine();
-                if (line.length() == 0)
-                {
-                    bodyEnded = true;
-                }
-                else
-                {
-                    request.append(line);
-                    request.append("\n");
-                }
-            }
-        }
+        int length = Integer.parseInt(headers.get("Content-Length"));
+        char[] buffer = new char[length];
+        br.read(buffer, 0, length);
+        request.append(buffer);
     }
 }
