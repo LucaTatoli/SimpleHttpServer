82abe5590b4773c136101e1aecb299e8ee0fe055
LucaTatoli <luca.tatoli27@gmail.com>
2022-12-24 01:40:20 +0100

    added very basic body detector

--- src/main/java/ServerEntryPoint.java
+++ src/main/java/ServerEntryPoint.java
@@ -1,70 +1,111 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
-public class ServerEntryPoint
-{
+import static javax.swing.text.html.FormSubmitEvent.MethodType.POST;
+
+public class ServerEntryPoint {
     private ServerSocket serverSocket;
     private boolean running = false;
+    private HashMap<String, String> headers;
 
-    public ServerEntryPoint(int port)
-    {
+    public ServerEntryPoint(int port) {
         try
         {
             serverSocket = new ServerSocket(port);
-        }
-        catch (IOException e)
+        } catch (IOException e)
         {
             e.printStackTrace();
         }
+        headers = new HashMap<>();
     }
 
-    public void listen()
-    {
+    public void listen() {
         running = true;
         Socket socket;
-        String newConnectionString = "==========================================\nNew connection from: %s\n==========================================\n";
+        String newConnectionString = "==========================================\nNew connection from: %s\n==========================================\n\n\n";
         String endConnectionString = "==========================================\nEnd connection with: %s\n==========================================\n\n\n";
-        while(running)
-        {
-            try
-            {
+        while (running) {
+            try {
                 socket = serverSocket.accept();
                 String addr = socket.getInetAddress().toString();
                 System.out.printf(newConnectionString, addr);
                 requestHandler(socket);
                 System.out.printf(endConnectionString, addr);
                 socket.close();
-            }
-            catch (IOException e)
-            {
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
 
-    public void requestHandler(Socket connection) throws IOException
-    {
+    public void requestHandler(Socket connection) throws IOException {
         String request = getRequest(connection.getInputStream());
-        String method = request.substring(0, request.indexOf("\n")).split(" ")[0];
+        String http_method = request.substring(0, request.indexOf("\n")).split(" ")[0];
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
-        if(!ApplicationConstants.HTTP_METHODS.contains(method))
+        if (!ApplicationConstants.HTTP_METHODS.contains(http_method))
             bw.write(HttpResponse.methodNotSupportedResponse());
         else
             bw.write(HttpResponse.okResponse());
         bw.flush();
+        System.out.println(request);
     }
 
-    public String getRequest(InputStream stream) throws IOException
-    {
+    public String getRequest(InputStream stream) throws IOException {
         StringBuilder request = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(stream));
-        while (br.ready())
-        {
-            request.append(br.readLine());
-            request.append("\n");
-        }
-        System.out.println(request);
+        getHeaders(br, request);
+        if(headers.containsKey("Content-Length") || headers.containsKey("Transfer-Encoding"))
+            getBody(br, request);
         return request.toString();
+    }
+
+    public void getHeaders(BufferedReader br, StringBuilder request) throws IOException
+    {
+        String line;
+        boolean headerEnded = false;
+        boolean skipFirst = true;
+        String[] split;
+        while (!headerEnded) {
+            if (br.ready())
+            {
+                line = br.readLine();
+                if (line.length() == 0)
+                {
+                    headerEnded = true;
+                }
+                else if(!skipFirst)
+                {
+                    split = line.split(":");
+                    headers.put(split[0], split[1]);
+                }
+                else skipFirst = false;
+                request.append(line);
+                request.append("\n");
+            }
+        }
+    }
+
+    public void getBody(BufferedReader br, StringBuilder request) throws IOException
+    {
+        String line;
+        boolean bodyEnded = false;
+        while (!bodyEnded) {
+            if (br.ready())
+            {
+                line = br.readLine();
+                if (line.length() == 0)
+                {
+                    bodyEnded = true;
+                }
+                else
+                {
+                    request.append(line);
+                    request.append("\n");
+                }
+            }
+        }
     }
 }
